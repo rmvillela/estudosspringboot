0fc0d5ee708294202858641596b63ada76a3e8c7
Roosevelt Villela <rmvillela@outlook.com>
2024-08-24 12:57:34 -0300

    Criação do ENUM Sexo e da Classe Cliente de acordo com o diagrama

---
+++ src/main/java/br/com/springboot/model/Cliente.java
@@ -0,0 +1,65 @@
+package br.com.springboot.model;
+
+import java.time.LocalDate;
+
+public class Cliente {
+	private Long id;
+	private String nome;
+	private String cpf;
+	private LocalDate dataDeNascimento;
+	private String telefone;
+	private String celular;
+	private String email;
+	private Boolean ativo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Boolean getAtivo() {
+		return ativo;
+	}
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
